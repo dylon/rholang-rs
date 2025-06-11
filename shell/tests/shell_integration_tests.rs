323c2b3fe67253bacc1620694f0bf2a94d02e10e
beret <beret@hipisi.org.pl>
2025-06-11 14:52:33 +0100

    remove outdated tests

--- shell/tests/shell_integration_tests.rs
+++
@@ -1,4 +0,0 @@
-// This file previously contained integration tests that required running the full binary.
-// These tests were removed because they were failing and required significant maintenance.
-// If integration testing is needed, consider using a more robust approach that doesn't
-// rely on spawning child processes and parsing their output.
