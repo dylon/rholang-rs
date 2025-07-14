c84e261ad8af0f538f2aee60fb58ba9fc48852c9
MarcinR-DevBrother <marcin.r@devbrother.com>
2025-07-14 04:17:10 +0200

    Add useful conversions

--- rholang-parser/src/lib.rs
+++ rholang-parser/src/lib.rs
@@ -26,11 +26,29 @@ impl Display for SourcePos {
     }
 }
 
-/// a span in the source code (inclusive)
+impl From<tree_sitter::Point> for SourcePos {
+    fn from(value: tree_sitter::Point) -> Self {
+        SourcePos {
+            line: value.row + 1,
+            col: value.column + 1,
+        }
+    }
+}
+
+/// a span in the source code (exclusive)
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SourceSpan {
     pub start: SourcePos,
     pub end: SourcePos,
+}
+
+impl From<tree_sitter::Range> for SourceSpan {
+    fn from(value: tree_sitter::Range) -> Self {
+        SourceSpan {
+            start: value.start_point.into(),
+            end: value.end_point.into(),
+        }
+    }
 }
 
 impl Display for SourceSpan {
